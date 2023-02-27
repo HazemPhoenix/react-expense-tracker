cb4e9556ffc3a5df90bab9eb2bebf722280f4590
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2023-02-27 20:51:48 +0200

    first custom component

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>Feb. 22nd 2023</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Mechanical Keyboard</h2>
-        <div className="expense-item__price">$50</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
