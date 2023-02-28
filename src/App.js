17d1ddd86aafa6b9e7b78b6d34e0a6870118239a
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2023-02-28 15:11:10 +0200

    add Expenses.jsx component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import ExpenseItem from "./Components/ExpenseItem";
-
+import Expenses from "./Components/Expenses";
 function App() {
   const expenses = [
     {
@@ -23,28 +22,8 @@ function App() {
     },
   ];
   return (
-    <div>
-      <div>
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        ></ExpenseItem>
-      </div>
-      <div>
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        ></ExpenseItem>
-      </div>
-      <div>
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        ></ExpenseItem>
-      </div>
+    <div className="container">
+      <Expenses expenses={expenses} />
     </div>
   );
 }
---
+++ src/Components/ExpenseDate.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ExpenseDate.css";
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  return (
+    <div>
+      <div className="expense-date">
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date_year">{year}</div>
+        <div className="expense-date__day">{day}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
